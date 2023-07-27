33c2618aca35e1802ad5b7d46ee3f463e864be08
Jordan Russo <114265336+Jordan-Russo@users.noreply.github.com>
2023-07-27 18:56:04 -0400

    Revert "Banner background"

--- routes/profiles.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const profilesController = require("../controllers/profiles")
-const { ensureAuth } = require("../middleware/auth");
-
-router.get("/:id", ensureAuth, profilesController.getProfile);
-router.get("/", ensureAuth, profilesController.getProfile);
-
-module.exports = router;
